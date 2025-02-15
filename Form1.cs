b137c0692f46dc3edd991cfe0ab3436b9790700f
Matei_Dana <dana.matei@stud.ubbcluj.ro>
2025-02-16 00:31:35 +0200

    populate comboxes

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,30 @@ namespace CurrencyXChange
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> currencies = new List<string>
+            {
+                "USD", // US Dollar
+                "EUR", // Euro
+                "GBP", // British Pound
+                "INR", // Indian Rupee
+                "AUD", // Australian Dollar
+                "CAD", // Canadian Dollar
+                "JPY", // Japanese Yen
+                "CNY"  // Chinese Yuan
+            };
+
+            // populate "fromCurrency" combox
+            cmbFromCurrency.DataSource = new List<string>(currencies);
+
+            // populate "toCurrency" combox
+            cmbToCurrency.DataSource = new List<string>(currencies);
+
+            // set default selections
+            cmbFromCurrency.SelectedItem = "USD";
+            cmbToCurrency.SelectedItem = "EUR";
+        }
     }
 }
